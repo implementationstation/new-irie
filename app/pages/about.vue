0a0dbc9f92ff4db6cc6f8684de0e07025ff14f80
Konrad Hugo <konradcoding@gmail.com>
2026-05-16 02:09:33 -0600

    setup pages

---
+++ app/pages/about.vue
@@ -0,0 +1,10 @@
+<!-- app/pages/about.vue -->
+<script setup>
+definePageMeta({
+  title: 'About'
+})
+</script>
+
+<template>
+  <h1>About Us</h1>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<!-- app/pages/about.vue -->
+<script setup>
+definePageMeta({
+  title: 'Home'
+})
+</script>
+
+<template>
+  <h1>Home</h1>
+  <p>Hi, kpop</p>
+
+</template>
